681573efb272e4ed0c407177ad2d68ec401e1e95
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-10 12:31:43 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,10 +6,12 @@ def create_project_hash
 
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  
 end
-
+binding.pry
 
 
 
 create_project_hash
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
