b8c9860c42f2848ab9ad08bbb65ef887f7650a22
Wali Rahman <wali.rahman89@yahoo.com>
2018-01-10 12:11:06 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def create_project_hash
 
-  html = File.read('fictures/kickstarter.html')
+  html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
